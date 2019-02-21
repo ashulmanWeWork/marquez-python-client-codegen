50e2d3818a2d8d77fb4434081448ba0c8438e6f0
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-02-21 11:09:47 -0800

    More commits of auto-gen'd code

---
+++ marquez_codegen_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from marquez_codegen_client.api.datasets_api import DatasetsApi
+from marquez_codegen_client.api.jobs_api import JobsApi
+from marquez_codegen_client.api.namespaces_api import NamespacesApi
